4b3d2588ff7d952e1d25e26d2eaef930b9590eef
Justin Ochoa <just.och92@gmail.com>
2022-02-06 14:47:22 -0500

    trying something out

--- index.js
+++ index.js
@@ -1,81 +1,85 @@
-let targetWords = fetch("./targetWords.json")
-  .then((response) => response.json())
-  .then((data) => data[Math.floor(dayOffset)]);
-let targeWord = await targetWords;
-console.log(targeWord);
+async function getWord() {
+  const guessGrid = document.querySelector("[data-guess-grid]");
+  const WORD_LENGTH = 5;
+  const offsetFromDate = new Date(2022, 0, 1);
+  const msOffset = Date.now() - offsetFromDate;
+  const dayOffset = msOffset / 1000 / 60 / 60 / 24;
 
-let dictionary = fetch("./dictionary.json")
-  .then((response) => response.json())
-  .then((data) => data);
+  let targetWord = await fetch("./targetWords.json")
+    .then((response) => response.json())
+    .then((data) => data[Math.floor(dayOffset)]);
 
-const guessGrid = document.querySelector("[data-guess-grid]");
-const WORD_LENGTH = 5;
-const offsetFromDate = new Date(2022, 0, 1);
-const msOffset = Date.now() - offsetFromDate;
-const dayOffset = msOffset / 1000 / 60 / 60 / 24;
+  console.log(targetWord);
 
-startInteraction();
+  let dictionary = fetch("./dictionary.json")
+    .then((response) => response.json())
+    .then((data) => data);
 
-function startInteraction() {
-  document.addEventListener("click", handleMouseClick);
-  document.addEventListener("keydown", handleKeyPress);
-}
+  startInteraction();
 
-function stopInteraction() {
-  document.removeEventListener("click", handleMouseClick);
-  document.removeEventListener("keydown", handleKeyPress);
-}
-
-function handleMouseClick(e) {
-  if (e.target.matches("[data-key]")) {
-    pressKey(e.target.dataset.key);
-    return;
+  function startInteraction() {
+    document.addEventListener("click", handleMouseClick);
+    document.addEventListener("keydown", handleKeyPress);
   }
-  if (e.target.matches("[data-enter]")) {
-    submitGuess();
-    return;
+
+  function stopInteraction() {
+    document.removeEventListener("click", handleMouseClick);
+    document.removeEventListener("keydown", handleKeyPress);
   }
-  if (e.target.matches("[data-delete]")) {
-    deleteKey();
-    return;
+
+  function handleMouseClick(e) {
+    if (e.target.matches("[data-key]")) {
+      pressKey(e.target.dataset.key);
+      return;
+    }
+    if (e.target.matches("[data-enter]")) {
+      submitGuess();
+      return;
+    }
+    if (e.target.matches("[data-delete]")) {
+      deleteKey();
+      return;
+    }
+  }
+
+  function handleKeyPress(e) {
+    if (e.key === "Enter") {
+      submitGuess();
+      return;
+    }
+    if (e.key === "Backspace" || e.key === "Delete") {
+      deleteKey();
+      return;
+    }
+    if (e.key.match(/^[a-z]$/)) {
+      pressKey(e.key);
+    }
+  }
+
+  function pressKey(key) {
+    const activeTiles = getActiveTiles();
+    if (activeTiles.length >= WORD_LENGTH) return;
+    const nextTile = guessGrid.querySelector(":not([data-letter])");
+    nextTile.dataset.letter = key.toLowerCase();
+    nextTile.textContent = key;
+    nextTile.dataset.state = "active";
+  }
+
+  function deleteKey() {
+    const activeTiles = getActiveTiles();
+    const lastTile = activeTiles[activeTiles.length - 1];
+    if (lastTile === null) return;
+    lastTile.textContent = "";
+    delete lastTile.dataset.state;
+    delete lastTile.dataset.letter;
+  }
+
+  function submitGuess() {
+    const activeTiles = [...getActiveTiles()];
+  }
+  function getActiveTiles() {
+    return guessGrid.querySelectorAll('[data-state="active"]');
   }
 }
 
-function handleKeyPress(e) {
-  if (e.key === "Enter") {
-    submitGuess();
-    return;
-  }
-  if (e.key === "Backspace" || e.key === "Delete") {
-    deleteKey();
-    return;
-  }
-  if (e.key.match(/^[a-z]$/)) {
-    pressKey(e.key);
-  }
-}
-
-function pressKey(key) {
-  const activeTiles = getActiveTiles();
-  if (activeTiles.length >= WORD_LENGTH) return;
-  const nextTile = guessGrid.querySelector(":not([data-letter])");
-  nextTile.dataset.letter = key.toLowerCase();
-  nextTile.textContent = key;
-  nextTile.dataset.state = "active";
-}
-
-function deleteKey() {
-  const activeTiles = getActiveTiles();
-  const lastTile = activeTiles[activeTiles.length - 1];
-  if (lastTile === null) return;
-  lastTile.textContent = "";
-  delete lastTile.dataset.state;
-  delete lastTile.dataset.letter;
-}
-
-function submitGuess() {
-  const activeTiles = [...getActiveTiles()];
-}
-function getActiveTiles() {
-  return guessGrid.querySelectorAll('[data-state="active"]');
-}
+getWord();
